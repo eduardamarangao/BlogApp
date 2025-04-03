62eb6084a7eec0d76afb0b17efb9eb95143660d0
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-04-03 15:27:11 -0400

    Added new codes

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router = express.Router();
+import express from 'express'
+const router = express.Router()
 
 router.get("/", (req, res) => {
     res.send('Página principal do painel adm')
