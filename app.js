adae42d39fc9c6cf8350e8be2492422c1b914fae
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-04-02 20:36:58 -0400

    Added new codes

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 // Carregando módulos
-const express = require ('express');
-const handlebars = require ('express-handlebars');
-const bodyParser = require ('body-parser');
-//const mongoose = require ('mongoose');
+    const express = require ('express');
+    const handlebars = require ('express-handlebars');
+    const bodyParser = require ('body-parser');
+    const app = express();
+    //const mongoose = require ('mongoose');
+// Configurações
+
+
+
+// Rotas
+
+
+// Outros
+const PORT = 8081;
+app.listen(PORT, () => {
+    console.log('Servidor rodando!');
+})
