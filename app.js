d1a7d689e48c05d55b80df387b9c9ba72143876c
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-04-15 19:22:54 -0400

    Added new codes

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
 // Carregando módulos
-    const express = require ('express');
-    const handlebars = require ('express-handlebars');
-    const bodyParser = require ('body-parser');
+    const express = require("express");
+    const handlebars = require("express-handlebars");
+    const bodyParser = require("body-parser");
     const app = express();
-    const admin = require ('./routes/admin');
-    const path = require ('path');
-    const mongoose = require ('mongoose');
+    const admin = require("./routes/admin");
+    const path = require("path");
+    const mongoose = require("mongoose");
 // Configurações
-    // Body Parser
-    app.use(express.urlencoded({extended: true}));
-    app.use(express.json());
-    // Handlebars
-        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
-        app.set('view engine', 'handlebars');
-//Mongoose
-    mongoose.connect('mongodb://localhost/blogapp').then(() => {
-        console.log('Conectado ao MongoDB!');
-    }).catch((err) => {
-        console.log('Erro ao se conectar ao MongoDB: ' + err);
-    });
-    // Em breve
-//Public 
-    app.use(express.static(path.join(__dirname, 'public')))
 
+// Body Parser
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+// Handlebars
+    app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
+    app.set("view engine", "handlebars");
+//Mongoose
+    mongoose
+    .connect("mongodb://localhost/blogapp")
+    .then(() => {
+        console.log("Conectado ao MongoDB!");
+    })
+    .catch((err) => {
+        console.log("Erro ao se conectar ao MongoDB: " + err);
+    });
+// Em breve
+//Public
+    app.use(express.static(path.join(__dirname, "public")));
 
 // Rotas
-    app.get('/', (req, res) => {
-        res.send('Página inicial.');
+    app.get("/", (req, res) => {
+    res.send("Página inicial.");
     });
 
-    app.get('/admin', (req, res) => {    
-        res.render("admin/index")
+    app.get("/admin", (req, res) => {
+    res.render("admin/index");
     });
 
-    app.get('/posts', (req, res) => {
-        res.send('lista de posts.');
+    app.get("/posts", (req, res) => {
+    res.send("lista de posts.");
     });
 
-    app.use('/admin', admin);   
+    app.use("/admin", admin);
 // Outros
-const PORT = 8081;
-app.listen(PORT, () => {
-    console.log('Servidor rodando na url http://localhost:8081!');
-})
+    const PORT = 8081;
+    app.listen(PORT, () => {
+    console.log("Servidor rodando na url http://localhost:8081!");
+    });
